47622d0951c9b04295950be3e0c69eee869a0342
lyal1ch1 <106307360+lyal1ch1@users.noreply.github.com>
2022-12-19 15:47:12 +0300

    Add files via upload

---
+++ Zadacha41.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Сколько чисел вы хотите ввести?");
+int length = int.Parse(Console.ReadLine()!);
+int count = 0;
+for (int i = 0; i < length; i++)
+{
+    Console.WriteLine("Введите " + (i+1) + "-ое число");
+    int num = int.Parse(Console.ReadLine()!);
+    if (num > 0)
+    {
+        count++;
+    }
+}
+Console.WriteLine("Вы ввели " + count + " числа больше 0");

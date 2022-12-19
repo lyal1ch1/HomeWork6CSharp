e360ef98b328ef7c9f6798d13ca8c47335ce7466
lyal1ch1 <106307360+lyal1ch1@users.noreply.github.com>
2022-12-19 15:59:06 +0300

    Update Zadacha41.cs

--- Zadacha41.cs
+++ Zadacha41.cs
@@ -1,3 +1,4 @@
+
 Console.WriteLine("Сколько чисел вы хотите ввести?");
 int length = int.Parse(Console.ReadLine()!);
 int count = 0;
@@ -10,15 +11,15 @@ for (int i = 0; i < length; i++)
         count++;
     }
 }
-if (count == 0 || count >= 5)
+if (count % 10 == 0 || count % 10 >= 5)
 {
    Console.WriteLine("Вы ввели " + count + " чисел больше 0"); 
 } 
-if (count == 1)
+if (count % 10 == 1)
 {
  Console.WriteLine("Вы ввели " + count + " число больше 0");
 } 
-if (count > 1 && count < 5 )
+if (count % 10 > 1 && count % 10 < 5 )
 {
  Console.WriteLine("Вы ввели " + count + " числа больше 0");
 } 
